517d2078b43a60239673481792853dd6ff5a3e20
Muhammad Abdullah <mabdulah596@hotmail.com>
2019-09-18 15:02:55 +0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './shared/header/header.component';
+import { LoginComponent } from './pages/login/login.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MainpageComponent } from './pages/mainpage/mainpage.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    DashboardComponent,
+    MainpageComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
