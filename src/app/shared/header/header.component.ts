5129a15e558d3ab1ec664dc5636f9016bf1af592
Muhammad Abdullah <mabdulah596@hotmail.com>
2019-09-18 20:03:08 +0500

    update

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -9,6 +9,8 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   @ViewChild('side', {static: false}) side: ElementRef;
   @ViewChild('nav', {static: false}) nav: ElementRef;
+  @ViewChild('header', {static: false}) header: ElementRef;
+  @ViewChild('sidenav', {static: false}) sidenav: ElementRef;
   open=false;
   constructor(public renderer: Renderer,private router:Router) { }
 
@@ -17,20 +19,51 @@ export class HeaderComponent implements OnInit {
   }
   openSideNav(){
    console.log(this.side.nativeElement.className);
-   if(this.open==false){
-     this.open=true;
+   if(this.sidenav.nativeElement.className=='className','enable-hover side'){
+    this.renderer.setElementProperty(this.header.nativeElement, 'className','navbar navbar-default navbar-fixed-top');
+    this.renderer.setElementProperty(this.sidenav.nativeElement, 'className','enable-hover');
+   }
+   if(this.side.nativeElement.className !='active'){
+
     this.renderer.setElementProperty(this.side.nativeElement, 'className','active');
     this.renderer.setElementProperty(this.nav.nativeElement, 'className','header-fixed-top sidebar-left-open');
    }
-   else{
-     this.open=false;
+   else if(this.side.nativeElement.className =='active'){
+console.log("ere");
     this.renderer.setElementProperty(this.side.nativeElement, 'className','');
     this.renderer.setElementProperty(this.nav.nativeElement, 'className','header-fixed-top ');
    }
 
   }
   dashboard(){
-    this.renderer.setElementProperty(this.side.nativeElement, 'className','');
-    this.renderer.setElementProperty(this.nav.nativeElement, 'className','header-fixed-top ');
+    this.renderer.setElementProperty(this.header.nativeElement, 'className','navbar navbar-default navbar-fixed-top head');
+    this.renderer.setElementProperty(this.sidenav.nativeElement, 'className','enable-hover side');
+      this.router.navigate(['/dashboard/mainpage']);
+      console.log("object");
+
+  }
+  employee(){
+    this.renderer.setElementProperty(this.header.nativeElement, 'className','navbar navbar-default navbar-fixed-top head');
+    this.renderer.setElementProperty(this.sidenav.nativeElement, 'className','enable-hover side');
+    this.router.navigate(['/employeeList']);
+    console.log("object");
+  }
+  company(){
+    this.renderer.setElementProperty(this.header.nativeElement, 'className','navbar navbar-default navbar-fixed-top head');
+    this.renderer.setElementProperty(this.sidenav.nativeElement, 'className','enable-hover side');
+    this.router.navigate(['/companylist']);
+    console.log("object");
+  }
+  job(){
+    this.renderer.setElementProperty(this.header.nativeElement, 'className','navbar navbar-default navbar-fixed-top head');
+    this.renderer.setElementProperty(this.sidenav.nativeElement, 'className','enable-hover side');
+    this.router.navigate(['/joblist']);
+    console.log("object");
+  }
+  business(){
+    this.renderer.setElementProperty(this.header.nativeElement, 'className','navbar navbar-default navbar-fixed-top head');
+    this.renderer.setElementProperty(this.sidenav.nativeElement, 'className','enable-hover side');
+    this.router.navigate(['/businesslist']);
+    console.log("object");
   }
 }
